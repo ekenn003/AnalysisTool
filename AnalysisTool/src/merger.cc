e7c70e2af24d251ef549e7476b25e75b97701cab
E Kennedy <ekennedy@cern.ch>
2016-01-27 14:10:08 +0100

    merger fix

--- AnalysisTool/src/merger.cc
+++ AnalysisTool/src/merger.cc
@@ -48,37 +48,44 @@ int main() {
     Float_t avgpu;
     UInt_t run;
     UInt_t block;
+    //TH1D* muhist = new TH1D("mudist", "mudist", 200, 0., 100.);
     if (file.is_open()) {
         file.getline(line, 1000);
         while (!file.eof()) {
             file.getline(line, 1000);
             vector<std::string> strs;
-//            file.getline(line,1000);
-//            vector<std::string> strs;
-//            boost::split(strs, line, boost::is_any_of(","));
-//            if(strs.size() != 8) continue;
-//            run = atoi(strs[0].c_str());
-//            vector<std::string> blocks;
-//            boost::split(blocks, strs[1], boost::is_any_of(":"));
-//            block = atoi(blocks[0].c_str());
-//            lumirecorded = atof(strs[6].c_str());
-//            avgpu = atof(strs[7].c_str());
+/*
+            file.getline(line,1000);
+            vector<std::string> strs;
+            boost::split(strs, line, boost::is_any_of(","));
+            if(strs.size() != 8) continue;
+            run = atoi(strs[0].c_str());
+            vector<std::string> blocks;
+            boost::split(blocks, strs[1], boost::is_any_of(":"));
+            block = atoi(blocks[0].c_str());
+            lumirecorded = atof(strs[6].c_str());
+            avgpu = atof(strs[7].c_str());
+*/
 
             boost::split(strs, line, boost::is_any_of("|"));
-            if (strs.size() != 6) continue;
+            if (strs.size() != 11) continue;
             vector<std::string> runfill;
-            boost::split(runfill, strs[0], boost::is_any_of(":"));
+            vector<std::string> lumisec;
+            boost::split(runfill, strs[1], boost::is_any_of(":"));
+            boost::split(lumisec, strs[2], boost::is_any_of(":"));
             run = atoi(runfill[0].c_str());
-            block = atoi(strs[2].c_str()); // not sure if this should be "nls" or "ncms"
-            lumirecorded = atof(strs[5].c_str());
+            block = atoi(lumisec[0].c_str()); // not sure if this should be "nls" or "ncms"
+            lumirecorded = atof(strs[7].c_str());
+            avgpu = atof(strs[8].c_str());
+
             if (lumirecorded == 0.) {
                 cout << "WARNING Lumi value is 0. Run: " << run << ", Block: " << block << endl;
                 continue;
             }
-//            if (ana.SetLumi(run, block, lumirecorded/1000000., avgpu) == 1) {
-//                //muhist->Fill(atof(strs[7].c_str())*766./693.);
-//                muhist->Fill(avgpu);
-//            }
+            //if (ana.SetLumi(run, block, lumirecorded/1000000., avgpu) == 1) {
+            //    //muhist->Fill(atof(strs[8].c_str())*766./693.);
+            //    muhist->Fill(avgpu);
+            //}
         }
         file.close();
     } else {
